FIX: distance to centroid calculation
Data matrix using columns as observation instead of rows, causing two points to be different dimensions.

kmeans.py
@@ -25,6 +25,7 @@ def kmeans(data, k):
     membership = Series(index=data.index)
 
     # algorithm converges when the memberships and centroids stop changing
+    num = 0
     converged = False
     while(not converged):
         # calculate distance between all data and centers (matricies)
@@ -34,11 +35,13 @@ def kmeans(data, k):
 
         for i in range(0,k):
             # calculate the euclidean distance to each centroid
-            distance[i] = np.sqrt(np.sum(np.power(data[i,:] - centroids[i], 2), axis=1))
+            distance.loc[:,i] = np.sqrt(np.sum(np.power(data.values - centroids[i], 2), axis=1))
+            print(distance)
 
-            # assign group memberships
-            closest_indicies = distance[i].idxmin()
-            membership.loc[closest_indicies] = i
+        # assign group memberships
+        closest_indicies = distance[i].idxmin()
+        membership.loc[closest_indicies] = i
+        print(membership)
 
         #calcuate new group centroids
         old_centroids = centroids.copy()
@@ -47,8 +50,40 @@ def kmeans(data, k):
         centroids = data.mean(axis=0).values
 
         # check for convergence
+        print(np.sum(centroids == old_centroids))
         if np.sum(centroids == old_centroids) == k:
-            converged = True        
+            converged = True
+        if num == 5:
+            converged = True
+        else:
+            num += 1
     
     return centroids, memberships
 
+
+def euclidian_distance(point1, point2):
+    '''
+    find the euclidean distance between two points
+    ---
+    inputs:
+        @param point1: a Series for a data observation
+        @param point2: a Series for a different data observation
+    ---
+    output:
+        the euclidean distance between the two points
+    ---
+    Assumptions:
+        - point1 and point2 should be same dimension
+        - point1 and point2 should not contain NaN
+    Formula for Euclidean Distance:
+        dist = sqrt(sum((a[1]+b[1])^2 + ... + (a[n]+b[n])^2))
+    '''
+    if len(point1) != len(point2):
+        raise ValueException("points should be same dimension")
+    
+    sum_distance = 0;
+    for i in range(0, len(point1)):
+        # determine the distance in the current dimension
+        distance_part = 0
+
+    return np.sqrt(sum_distance)